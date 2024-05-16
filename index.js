f3b994986e96e0c9b2c852b8061c7218e64980ff
smasha11 <ismail.hussein.1120@gmail.com>
2024-05-16 10:59:18 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  console.log(saturdayFun());
+  
+  function mondayWork(activity='go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  console.log(mondayWork());
+  
+  function wrapAdjective(highlight = '*') {
+    return function(adjective) {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    };
+  }
+  
